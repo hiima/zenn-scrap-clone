beeeb4e4e6937bd8afd8da1d2972cef7dcaab670
hiima <h.iima@buysell-technologies.com>
2022-08-05 09:55:37 +0900

    chore(api): add seeds

---
+++ api/hasura/seeds/default/1659660891812_scraps.sql
@@ -0,0 +1,4 @@
+SET check_function_bodies = false;
+INSERT INTO public.scraps (id, title, posted_at) VALUES ('a1cb5154-abeb-4e64-8955-682382cd747a', 'Remixの素振り', '2022-08-05 00:00:00+09');
+INSERT INTO public.scraps (id, title, posted_at) VALUES ('a57790ac-7b24-418f-837b-ea5c2ec35dee', '今日の献立について考える', '2022-08-05 01:00:00+09');
+INSERT INTO public.scraps (id, title, posted_at) VALUES ('5e0e1593-5cff-4085-9712-8545afced1ed', 'リーダブルコード読書ログ', '2022-08-05 02:00:00+09');
