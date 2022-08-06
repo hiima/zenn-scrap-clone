522139de32817cffcd2489ac9ad1996a2f9bf2c1
hiima <h.iima@buysell-technologies.com>
2022-08-06 16:51:21 +0900

    chore(web): AppBar内の要素の最大幅を調整

--- web/src/components/Bar/Bar.tsx
+++ web/src/components/Bar/Bar.tsx
@@ -1,4 +1,5 @@
 import AppBar from "@mui/material/AppBar";
+import Container from "@mui/material/Container";
 import Link from "@mui/material/Link";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
@@ -7,13 +8,15 @@ import React from "react";
 export const Bar: React.FC = () => {
   return (
     <AppBar position="static" sx={{ mb: "1rem" }}>
-      <Toolbar>
-        <Typography variant="h5">
-          <Link underline="none" color="inherit" href="/">
-            Skrap
-          </Link>
-        </Typography>
-      </Toolbar>
+      <Container maxWidth="md">
+        <Toolbar>
+          <Typography variant="h5">
+            <Link underline="none" color="inherit" href="/">
+              Skrap
+            </Link>
+          </Typography>
+        </Toolbar>
+      </Container>
     </AppBar>
   );
 };
