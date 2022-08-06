19aada1eba503931e1755b71d6ced4ae7ec1b4fc
hiima <h.iima@buysell-technologies.com>
2022-08-06 22:25:36 +0900

    feat(web): スクラップ画面でコメント一覧を表示する

---
+++ web/src/components/CommentCard/CommentCard.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+
+type CommentCardProps = {
+  content: string;
+  postedAt: string;
+};
+
+export const CommentCard: React.FC<CommentCardProps> = ({
+  content,
+  postedAt,
+}) => {
+  return (
+    <Card sx={{ mt: "0.3rem" }}>
+      <CardContent>
+        <Stack direction="row" justifyContent="space-between">
+          <Stack>
+            <Typography>
+              {/* NOTE: 改行コードがあればそこで改行するようにしている */}
+              <pre style={{ fontFamily: "inherit" }}>{content}</pre>
+            </Typography>
+            <Typography variant="subtitle2" color="gray">
+              {postedAt}
+            </Typography>
+          </Stack>
+        </Stack>
+      </CardContent>
+    </Card>
+  );
+};
