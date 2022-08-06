d207ac530072894c0cca4466b9714ca3371be7de
hiima <h.iima@buysell-technologies.com>
2022-08-06 18:41:51 +0900

    feat(web): コメント一覧の取得

--- web/src/pages/scraps/[scrapId]/index.tsx
+++ web/src/pages/scraps/[scrapId]/index.tsx
@@ -1,19 +1,38 @@
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
 import type { NextPage } from "next";
+import Error from "next/error";
 import { useRouter } from "next/router";
 import { Bar } from "../../../components/Bar";
+import { useCommentsQuery } from "../../../graphql/generated";
 
 const Scrap: NextPage = () => {
   const router = useRouter();
-  const { scrapId } = router.query;
+  const scrapId = router.query.scrapId as string;
+
+  const { data, loading, error } = useCommentsQuery({ variables: { scrapId } });
+
+  if (error) return <Error statusCode={500} />;
 
   return (
     <>
       <CssBaseline />
       <Bar />
       <Container maxWidth="md">
-        <p>{scrapId}</p>
+        <>
+          {!loading &&
+            data &&
+            data.comments.map((comment) => {
+              return (
+                <>
+                  <p>{comment.id}</p>
+                  <p>{comment.content}</p>
+                  <p>{comment.postedAt}</p>
+                  <p>===</p>
+                </>
+              );
+            })}
+        </>
       </Container>
     </>
   );
