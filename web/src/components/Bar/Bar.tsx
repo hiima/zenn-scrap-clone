a5be48b8e7e69182aa9f56a768aa782d58601d79
hiima <h.iima@buysell-technologies.com>
2022-08-07 17:22:05 +0900

    chore(web): バーのスタイル調整

--- web/src/components/Bar/Bar.tsx
+++ web/src/components/Bar/Bar.tsx
@@ -15,13 +15,16 @@ export const Bar: React.FC = () => {
   const handleAddNewClick = () => router.push("/scraps/new");
 
   return (
-    <AppBar position="static" sx={{ mb: "1rem", boxShadow: 0 }}>
+    <AppBar
+      position="static"
+      sx={{ mb: "1rem", boxShadow: 0, background: "white" }}
+    >
       <Container maxWidth="md">
         <Toolbar>
           {/* NOTE: 左側に配置 */}
           <Box display="flex" flexGrow={1}>
             <Typography variant="h5">
-              <Link underline="none" color="inherit" href="/">
+              <Link underline="none" color="primary" fontWeight="bold" href="/">
                 Skrap
               </Link>
             </Typography>
