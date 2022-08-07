b11bce25856ac91943bb90e4e7b599d369cf69cb
hiima <h.iima@buysell-technologies.com>
2022-08-07 14:44:09 +0900

    refactor(web): メニューコンポーネントを分離

--- web/src/components/CommentCardList/CommentCard.tsx
+++ web/src/components/CommentCardList/CommentCard.tsx
@@ -1,17 +1,11 @@
-import React, { useState } from "react";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
+import { useState } from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Stack from "@mui/material/Stack";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
 import Typography from "@mui/material/Typography";
 import { toRelativeDate } from "../../lib/toRelativeDate";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import DeleteIconOutlined from "@mui/icons-material/Delete";
-import EditIconOutlined from "@mui/icons-material/Edit";
 import { DeleteCommentConfirmDialog } from "../DeleteCommentConfirmDialog";
+import { CommentCardMenu } from "./CommentCardMenu";
 
 type CommentCardProps = {
   commentId: string;
@@ -27,22 +21,6 @@ export const CommentCard: React.FC<CommentCardProps> = ({
   postedAt,
   afterMutationCompleted,
 }) => {
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const menuOpen = Boolean(anchorEl);
-  const handleMenuClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-  };
-  const handleEditClick = () => {
-    setAnchorEl(null);
-  };
-  const handleDeleteClick = () => {
-    setDialogOpen(true);
-    setAnchorEl(null);
-  };
-
   const [dialogOpen, setDialogOpen] = useState(false);
 
   return (
@@ -60,30 +38,9 @@ export const CommentCard: React.FC<CommentCardProps> = ({
             <Typography variant="subtitle2" color="gray">
               {toRelativeDate(postedAt)}
             </Typography>
-            <Box display="flex" justifyContent="flex-end">
-              <Button
-                disableRipple
-                style={{
-                  backgroundColor: "transparent",
-                }}
-                endIcon={<KeyboardArrowDownIcon sx={{ color: "gray" }} />}
-                onClick={handleMenuClick}
-              />
-              <Menu
-                anchorEl={anchorEl}
-                open={menuOpen}
-                onClose={handleMenuClose}
-              >
-                <MenuItem disableRipple onClick={handleEditClick}>
-                  <EditIconOutlined />
-                  編集
-                </MenuItem>
-                <MenuItem disableRipple onClick={handleDeleteClick}>
-                  <DeleteIconOutlined sx={{ color: "red" }} />
-                  <Typography color="red">削除</Typography>
-                </MenuItem>
-              </Menu>
-            </Box>
+
+            <CommentCardMenu onClickDelete={() => setDialogOpen(true)} />
+
             <Typography
               sx={{
                 wordWrap: "break-word",
---
+++ web/src/components/CommentCardList/CommentCardMenu.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Typography from "@mui/material/Typography";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import DeleteIconOutlined from "@mui/icons-material/Delete";
+import EditIconOutlined from "@mui/icons-material/Edit";
+
+type CommentCardMenuProps = {
+  /** 編集ボタンクリック時に実行するコールバック */
+  // onClickEdit: () => void;
+  /** 削除ボタンクリック時に実行するコールバック */
+  onClickDelete: () => void;
+};
+
+export const CommentCardMenu: React.FC<CommentCardMenuProps> = ({
+  onClickDelete,
+}) => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const menuOpen = Boolean(anchorEl);
+  const handleMenuClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+  const handleEditClick = () => {
+    setAnchorEl(null);
+  };
+  const handleDeleteClick = () => {
+    onClickDelete();
+    setAnchorEl(null);
+  };
+
+  return (
+    <Box display="flex" justifyContent="flex-end">
+      <Button
+        disableRipple
+        style={{
+          backgroundColor: "transparent",
+        }}
+        endIcon={<KeyboardArrowDownIcon sx={{ color: "gray" }} />}
+        onClick={handleMenuClick}
+      />
+      <Menu anchorEl={anchorEl} open={menuOpen} onClose={handleMenuClose}>
+        <MenuItem disableRipple onClick={handleEditClick}>
+          <EditIconOutlined />
+          編集
+        </MenuItem>
+        <MenuItem disableRipple onClick={handleDeleteClick}>
+          <DeleteIconOutlined sx={{ color: "red" }} />
+          <Typography color="red">削除</Typography>
+        </MenuItem>
+      </Menu>
+    </Box>
+  );
+};
